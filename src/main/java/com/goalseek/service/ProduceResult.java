bcb5853977f8ca9e12a29724579b31d864ad3bb4
sreejithmahe <sreejith.mahe@gmail.com>
2019-09-15 12:17:36 +0530

    GoalSeekFinder Final v.9

--- src/main/java/com/goalseek/service/ProduceResult.java
+++ src/main/java/com/goalseek/service/ProduceResult.java
@@ -1,13 +1,20 @@
 package com.goalseek.service;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author sreejith.kizhakkayil
  *
  */
 import org.springframework.stereotype.Service;
 
+import com.goalseek.dao.GoalSeekOutDao;
+
 @Service
 public class ProduceResult {
-	 public StringBuilder M1(double OP1,double Guess,double ExpCB,double NP) {
+	
+	 private List<GoalSeekOutDao>  getJsonResultP = new ArrayList<GoalSeekOutDao>(); 
+	 public List<GoalSeekOutDao> M1(double OP1,double Guess,double ExpCB,double NP) {
           double   Lfee1 = Guess;
           double IRPer1    = 0.06;
           double MarPer1   = 0.01;
@@ -23,26 +30,37 @@ public class ProduceResult {
           (OP1-Lfee1)*IRPer1+TotMarTLP1/(1-TotMarTLP1)*IRPer1*(OP1-Lfee1);
           double Depre1 = Lfee1-TLP1-Margin1-IR1;
           double CB1 = OP1-Depre1;
-		  StringBuilder GoalResultJson = new StringBuilder();
-          String Jopn ="{";
-          String op = "\"OPN-1.0\": "+Math.round(OP1)+",";
-          String dep = "\"DEP-1.0\": "+Math.round(Depre1)+",";
-          String cb = "\"CLB-1.0\": "+Math.round(CB1)+",";
-          String ir = "\"INR-1.0\": "+Math.round(IR1)+",";
-          String mar = "\"MAR-1.0\": "+Math.round(Margin1)+",";
-          String tlp = "\"TLP-1.0\": "+Math.round(TLP1)+",";
-          String lfe = "\"LEF-1.0\": "+Math.round(Lfee1)+",";
-          GoalResultJson.append(Jopn+"\n"+op+"\n"+dep+"\n"+cb+"\n"+ir+"\n"+mar+"\n"+tlp+"\n"+lfe+"\n");
-          StringBuilder MNsb = MN(CB1,Guess,ExpCB,NP);
-         return GoalResultJson.append(MNsb);
+		/*
+		 * StringBuilder GoalResultJson = new StringBuilder(); String Jopn ="{"; String
+		 * op = "\"OPN-1.0\": "+Math.round(OP1)+","; String dep =
+		 * "\"DEP-1.0\": "+Math.round(Depre1)+","; String cb =
+		 * "\"CLB-1.0\": "+Math.round(CB1)+","; String ir =
+		 * "\"INR-1.0\": "+Math.round(IR1)+","; String mar =
+		 * "\"MAR-1.0\": "+Math.round(Margin1)+","; String tlp =
+		 * "\"TLP-1.0\": "+Math.round(TLP1)+","; String lfe =
+		 * "\"LEF-1.0\": "+Math.round(Lfee1)+",";
+		 * GoalResultJson.append(Jopn+"\n"+op+"\n"+dep+"\n"+cb+"\n"+ir+"\n"+mar+"\n"+tlp
+		 * +"\n"+lfe+"\n");
+		 */
+          GoalSeekOutDao gs = new GoalSeekOutDao();
+          gs.setOpeningBalance(Math.round(OP1));
+          gs.setDepreciation(Math.round(Depre1));
+          gs.setClosingBalance(Math.round(CB1));
+          gs.setInterestRate(Math.round(IR1));
+          gs.setMargin(Math.round(Margin1));
+          gs.setTlp(Math.round(TLP1));
+          gs.setLeaseFee(+Math.round(Lfee1));
+          gs.setPeriod(1);
+          getJsonResultP.add(gs);
+          MN(CB1,Guess,ExpCB,NP);
+          return getJsonResultP;
     }
-	 public StringBuilder MN(double OPn,double Guess,double ExpCB,double NP) {
-	      double Period =2;
+	 public List<GoalSeekOutDao> MN(double OPn,double Guess,double ExpCB,double NP) {
+	      int Period =2;
 	      double OPN = OPn;
 	      double  LfeeN = 0;
 	      double CBN = 0;
-	      StringBuilder GoalResultJson = new StringBuilder();
-	           do {
+	            do {
 	           double IRPerN    = 0.06;
 	           double MarPerN   = 0.01;
 	           double TLPPerN   = 0.01;
@@ -58,24 +76,19 @@ public class ProduceResult {
 	           (OPN-LfeeN)*IRPerN+TotMarTLPN/(1-TotMarTLPN)*IRPerN*(OPN-LfeeN);
 	           double DepreN = LfeeN-TLPN-MarginN-IRN;
 	           CBN = OPN-DepreN;
-	           String op = "\"OPN-"+Period+"\": "+Math.round(OPN)+",";
-	           String dep = "\"DEP-"+Period+"\": "+Math.round(DepreN)+",";
-	           String cb = "\"CLB-"+Period+"\": "+Math.round(CBN)+",";
-	           String ir = "\"INR-"+Period+"\": "+Math.round(IRN)+",";
-	           String mar = "\"MAR-"+Period+"\": "+Math.round(MarginN)+",";
-	           String tlp = "\"TLP-"+Period+"\": "+Math.round(TLPN)+",";
-	           String lfe="";
-	           if(Period != NP) { 
-	            lfe = "\"LEF-"+Period+"\": "+Math.round(LfeeN)+",";
-	           } else  {
-	        	   lfe = "\"LEF-"+Period+"\": "+Math.round(LfeeN)+"";
-	           }
-	           GoalResultJson.append("\n"+op+"\n"+dep+"\n"+cb+"\n"+ir+"\n"+mar+"\n"+tlp+"\n"+lfe+"\n");
+	           GoalSeekOutDao gs = new GoalSeekOutDao();
+	           gs.setOpeningBalance(Math.round(OPN));
+	           gs.setDepreciation(Math.round(DepreN));
+	           gs.setClosingBalance(Math.round(CBN));
+	           gs.setInterestRate(Math.round(IRN));
+	           gs.setMargin(Math.round(MarginN));
+	           gs.setTlp(Math.round(TLPN));
+	           gs.setLeaseFee(+Math.round(LfeeN));
+	           gs.setPeriod(Period);
+		       getJsonResultP.add(gs);
 	             Period = Period+1;
 	             OPN = CBN;
 	        }while(Period <= NP);
-		       String Jclos = "}";
-		       GoalResultJson.append(Jclos);
-		       return GoalResultJson;
+		       return getJsonResultP;
 	}
 }
